8441cca36374f4efadb77ac3d6bab2f9766031f2
ztie <45815096+ztie@users.noreply.github.com>
2018-12-12 10:58:35 -0800

    Add files via upload

---
+++ MyProject-Chatbot/my_module/test_chatbot_functions.py
@@ -0,0 +1,20 @@
+"""Test for my functions."""
+
+from my_code import *
+
+def test_disorder_filter():
+
+    assert isinstance(disorder_filter(["what","is","the","cause","for", "depression"],DISORDERS, CATEGORIES),list)
+    assert disorder_filter(["what","is","the","cause","for", "depression"], DISORDERS, CATEGORIES) == ["depression", "cause"]
+                                      
+        
+def test_find_disorder():
+
+    assert isinstance(find_disorder(disorders,"bipolar"),object)
+    assert find_disorder(disorders, "bipolar") == bipolar
+    
+    
+def test_find_output():
+    
+    assert isinstance(find_output(disorders,["schizophrenia","symptom"]),str)
+    assert find_output(disorders,["schizophrenia","symptom"]) ==  '1. Having false beliefs\n        2. Hearing voices that others do not\n        3. Reduced emotional expression\n        4. Lack of motivation\n'
